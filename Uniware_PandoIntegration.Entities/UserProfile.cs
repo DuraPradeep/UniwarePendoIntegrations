3352af23590c7e1c66d92732b4d478965825a487
Ajaya Kumar Sahu <ajayakumar.s@gsr-inc.com>
2024-02-02 19:04:00 +0530

    Commit

---
+++ Uniware_PandoIntegration.Entities/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Uniware_PandoIntegration.Entities
+{
+    public class UserProfile
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public int Roleid { get; set; }
+        public SelectList RoleName { get; set; }
+    }
+}
