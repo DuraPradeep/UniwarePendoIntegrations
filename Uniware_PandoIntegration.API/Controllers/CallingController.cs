45e43325dfc7fd41623e205392a32777fef99581
Pradeep <cw.pradeep@duroflexworld.com>
2024-05-09 13:45:00 +0530

    Commit

---
+++ Uniware_PandoIntegration.API/Controllers/CallingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uniware_PandoIntegration.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallingController : ControllerBase
+    {
+    }
+}
